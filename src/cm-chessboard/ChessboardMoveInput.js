aeec749241c3c5682980826684c3e8f0b2c99d93
Adam Klein <adamklein@gmail.com>
2022-01-05 00:47:01 -0500

    Fix some issues

--- src/cm-chessboard/ChessboardMoveInput.js
+++ src/cm-chessboard/ChessboardMoveInput.js
@@ -200,7 +200,7 @@ export class ChessboardMoveInput {
         this.draggablePiece.name = pieceName
         const spriteUrl = this.chessboard.props.sprite.cache ? "" : this.chessboard.props.sprite.url
         const piece = Svg.addElement(this.draggablePiece, "use", {
-            href: `${spriteUrl}#sprite-${name}`
+            href: `${spriteUrl}#sprite-${name}${color}`
         })
         const scaling = this.view.squareHeight / this.chessboard.props.sprite.size
         const transformScale = (this.draggablePiece.createSVGTransform())
